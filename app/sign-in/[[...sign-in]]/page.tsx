c9a674b912288b69353bd0d276f83d668f2af640
NisargPatel14 <nisjp.14@gmail.com>
2024-06-29 11:41:57 +0530

    Updated sign-in page layout and styles

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,10 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center py-24">
-      <SignIn />
+    <div className="bg-black h-screen">
+      <div className="flex justify-center py-24">
+        <SignIn />
+      </div>
     </div>
   );
 }
