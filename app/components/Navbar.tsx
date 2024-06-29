54c7a4650ec38feed0e54b7704f231b67aae3e10
NisargPatel14 <nisjp.14@gmail.com>
2024-06-29 11:38:21 +0530

    Navbar Created

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,15 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex justify-between items-center px-10 bg-black text-white h-14'>
+      <h1 className='text-xl'>
+        Cypher Sheets
+      </h1>
+      <UserButton />
+    </nav>
+    
   )
 }
 
