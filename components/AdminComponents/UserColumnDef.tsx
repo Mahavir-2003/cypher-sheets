4b4d06725744e53f01acee4d8bcb12d9dcc3226b
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-06-29 15:52:33 +0530

    Displayed all users with thier roles

---
+++ components/AdminComponents/UserColumnDef.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal, ArrowUpDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export type User = {
+  id: string;
+  firstname: string;
+  lastname: string;
+  emailaddress: string;
+  role: string;
+};
+
+export const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: "id",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Id
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "firstname",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          First Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "lastname",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Last Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+
+  {
+    accessorKey: "emailaddress",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "role",
+    header: "Role",
+  },
+  {
+    accessorKey: "actions",
+    header: "Actions",
+    cell: (row) => (
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="ghost">
+            <MoreHorizontal />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Edit User Roles</DropdownMenuItem>
+          <DropdownMenuItem>Delete User</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    ),
+  },
+];
--- components/AdminComponents/Users.tsx
+++ components/AdminComponents/Users.tsx
@@ -1,8 +1,24 @@
+import { clerkClient } from '@clerk/nextjs/server';
 import React from 'react'
+import { DataTable } from './data-table';
+import { columns } from './UserColumnDef';
+function transformData(data: any[]) {
+  return data.map((user, index) => ({
+    id: (index + 1).toString(),
+    firstname: user.firstName || '',
+    lastname: user.lastName || '',
+    emailaddress: user.emailAddresses[0]?.emailAddress || '',
+    role: user.publicMetadata.role || '',
+  }));
+}
 
-const Users = () => {
+
+const Users = async () => {
+  const response = await clerkClient.users.getUserList();
+  //console.log(response.data);
+  const transformedData = transformData(response.data);
   return (
-    <div className=' w-full h-full bg-red-200'>Users</div>
+    <div className=' w-full h-full'><DataTable columns={columns} data={transformedData} /></div>
   )
 }
 
