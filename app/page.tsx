24ffb6d6f38c43bd64abfb600814d2d465da87c6
NisargPatel14 <nisjp.14@gmail.com>
2024-06-29 11:29:33 +0530

    Designed Home Screen.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <main className="flex flex-col gap-10 justify-center items-center w-screen h-screen bg-black">
+        <div className="inter text-white text-5xl font-bold">Cypher Sheets</div>
+        <div className="inter text-white text-xl">
+          A secure examination paper distribution system to
+          maintain the integrity and confidentiality of exam.
+        </div>
+        <Button variant={"secondary"}>
+          <Link href="/dashboard">Get Started</Link>
+        </Button>
+      </main>
     </>
   );
 }
