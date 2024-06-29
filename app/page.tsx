dad0ca26b51f21dfa23404e3a481ad86be157443
Dhruv-patel-1702 <111880388+Dhruv-patel-1702@users.noreply.github.com>
2024-06-29 14:17:52 +0530

    responsive page.tsx and Navbar.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,15 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col gap-10 justify-center items-center w-screen h-screen bg-black">
-        <div className="inter text-white text-5xl font-bold">Cypher Sheets</div>
-        <div className="inter text-white text-xl">
-          A secure examination paper distribution system to
-          maintain the integrity and confidentiality of exam.
+      <main className="flex flex-col gap-6 md:gap-10 justify-center items-center w-full h-screen bg-black px-4 md:px-0">
+        <div className="text-white text-3xl md:text-4xl lg:text-5xl 2xl:text-7xl font-bold text-center">
+          Cypher Sheets
         </div>
-        <Button variant={"secondary"}>
+        <div className=" text-white text-center max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl text-base md:text-lg lg:text-xl 2xl:text-2xl">
+          A secure examination paper distribution system to maintain the
+          integrity and confidentiality of exams.
+        </div>
+        <Button variant={"secondary"} className="hover:border border-gray-600">
           <Link href="/dashboard">Get Started</Link>
         </Button>
       </main>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,9 @@ import { ModeToggle } from "./ThemeToggle";
 
 const Navbar = () => {
   return (
-    <nav className="w-full flex justify-between items-center px-10 border-border border-[1px] rounded-md py-2">
+    <nav className="w-full flex justify-between items-center px-6 border-border border-[1px] rounded-md py-2 lg:px-10">
       <h1 className="text-xl font-semibold tracking-wide">Cypher Sheets</h1>
-      <div className="flex justify-center items-center gap-x-5">
+      <div className="flex justify-center items-center gap-x-4">
         <ModeToggle />
         <UserButton />
       </div>
